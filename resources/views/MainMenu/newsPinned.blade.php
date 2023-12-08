60fcbb9f2695cd6faf27ccbb526040f4681aa3fa
yoshihika <yoshihika1387@icloud.com>
2023-12-08 16:41:34 +0900

    最終

--- resources/views/MainMenu/newsPinned.blade.php
+++ resources/views/MainMenu/newsPinned.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ポータルサイト：お知らせ一覧</title>
+    <title>ポータルサイト：ピン留めしたお知らせ</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@4.0.0/destyle.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
